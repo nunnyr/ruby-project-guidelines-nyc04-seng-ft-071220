6dcebc602b1c43f3f7fc5e0486c96a27f5dbb48e
Nunnyr <nunnyreyes@gmail.com>
2020-08-17 19:51:29 -0400

    Building associations in our model(s)

--- bin/restaurant.rb
+++ bin/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
-
+    has_many :reservations
+    has_many :users, through: :reservations
+    belongs_to :precautions
+    #Annie please explain why this restaurant belongs to a 4th model.
+    #we are attempting a 4th model here. 
 end
